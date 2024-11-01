8897a365f5aef857e8148358709696be4f79c002
Skip R <tinyslices@gmail.com>
2024-11-01 13:42:19 -0700

    idk what happened last night

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ ExpressionLow
 Assign "assignment"
   = pattern:Pattern _sp "=" _sp value:(IndentedObjectLiteral / Expression) { return { type: 'Assignment', pattern, value }; }
 
-Samedent = spaces:$([ \\t]*) &{ console.log('current',indent.length, 'has',spaces.length); return spaces === indent; }
+Samedent = spaces:$([ \\t]*) &{ return spaces === indent; }
 Dedent = &{ indent = indentStack.pop(); return true; }
 Indent = spaces:$([ \\t]+) &{ return spaces.length > indent.length; } { indentStack.push(indent); indent = spaces; }
 
@@ -49,7 +49,8 @@ IndentedBlock
   / Block
 
 Call "function call"
-  = name:Target sp args:(Expression / IndentedObjectLiteral |1.., "," _sp|) { return { type: 'Call', name, args: array(args) }; }
+  = name:Target string:String { return { type: 'Call', name, args: [string] }; }
+  / name:Target sp args:(Expression / IndentedObjectLiteral |1.., ", " _sp|) { return { type: 'Call', name, args: array(args) }; }
   / name:Target "(" _sp args:(Expression |.., "," _sp|) _sp ")" { return { type: 'Call', name, args: array(args) }; }
 
 String "string"
@@ -63,6 +64,7 @@ IndentedObjectLiteral "indented object literal"
 
 KeyValue "key-value pair"
   = key:Target _sp ":" _sp value:(IndentedObjectLiteral / Expression) { return [key, value]; }
+  / key:Target args:Arguments _sp ":" _sp expr:(IndentedBlock / Expression) { return [key, { type: 'FunctionLiteral', args, expr}]; }
 
 Separator "newlines or ~"
   = "\\n" Samedent _
@@ -105,7 +107,7 @@ Factor
   / Integer
 
 Integer "integer"
-  = _ [0-9]+ { return Number.parseInt(text(), 10); }
+  = _ [0-9]+ { return { type: 'Integer', value: Number.parseInt(text(), 10) }; }
 
 _sp "spaces"
   = [ \\t]*
@@ -124,32 +126,65 @@ function exp(n) {
     case "Pattern":
       return exp(n.name);
     case "Identifier":
-      return n.name;
+      return n.name.replace(/-([a-zA-Z])/g, (_, m) => m.toUpperCase());
+    case "IndentedObjectLiteral":
+    case "ObjectLiteral":
+      return `{ ${n.kvs.map(([key, value]) => `${exp(key)}: ${exp(value)}`)} }`;
     case "PropertyTraversal":
       return `(${n.properties.map(exp).join(".")})`;
+    case "Block":
+    case "IndentedBlock":
+      return `(() => { ${n.exprs.map((sn, i) => (i === n.exprs.length - 1 ? "return " : "") + exp(sn)).join("\n")} })()`;
+    case "Addition":
+      return `(${exp(n.left)} + ${exp(n.right)})`;
     case "Call":
       return `${exp(n.name)}(${n.args.map(exp).join(",")})`;
     case "Program":
-      return n.exprs.map(exp).join("\n");
+      return n.exprs.map(exp).join(";\n");
+    case "Assignment":
+      return `let ${exp(n.pattern)} = ${exp(n.value)};`;
+    case "Integer":
+      return n.value;
     case "String":
       return JSON.stringify(n.text);
+    case "FunctionLiteral":
+      return `function(${n.args.map(exp).join(",")}) { return ${exp(n.expr)}; }`;
+    default:
+      throw new Error(`Unhandled AST node "${n.type}" :(`);
   }
 }
 
-const code = `say(text) = console\\log text
-say('yo')
-`;
+const filename = "./hi.~";
 
-const parsed = parser.parse(code);
+async function doit() {
+  console.clear();
+  console.log(`[${new Date().toLocaleString()}]`);
+  try {
+    const parsed = parser.parse(await Deno.readTextFile(filename));
+    console.log(
+      inspect(parsed, {
+        depth: Infinity,
+        colors: true,
+        compact: true,
+        numericSeparator: true,
+      })
+    );
 
-console.log(
-  inspect(parsed, {
-    depth: Infinity,
-    colors: true,
-    compact: true,
-    numericSeparator: true,
-  })
-);
+    console.log();
+    const compiled = exp(parsed);
+    console.log("Compiled:", compiled);
+    console.log();
+    console.log("-".repeat(50));
+    console.log();
 
-const compiled = exp(parsed);
-(0, eval)(compiled);
+    (0, eval)(compiled);
+  } catch (err) {
+    console.error("Failed to parse/evaluate:", err);
+  }
+}
+
+await doit();
+
+for await (const event of Deno.watchFs(filename)) {
+  await doit();
+}
